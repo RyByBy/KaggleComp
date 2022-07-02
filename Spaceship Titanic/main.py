cd4603834c2db84c86167b8b29beadb67bcc5e6e
Mateusz Śliwiński <253403@student.pwr.edu.pl>
2022-07-02 12:23:55 +0200

    Changed classifier for AdaBoost

--- Spaceship Titanic/main.py
+++ Spaceship Titanic/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import os
 from sklearn.preprocessing import LabelEncoder
-from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.utils import column_or_1d
 from sklearn.svm import SVC
@@ -105,13 +105,17 @@ X_train = X.drop(['Cabin','Transported'],axis=1)
 X_test = X_test.drop(['Cabin'],axis=1)
 
 '''Fitting and predicting into sklearn Bagging Classifier with GNB'''
-clf = BaggingClassifier(base_estimator=GaussianNB(), random_state=0)
+# clf = BaggingClassifier(base_estimator=GaussianNB(), random_state=0)
+clf = AdaBoostClassifier()
 clf.fit(X_train, y_train)
 pred = clf.predict(X_test)
 samples['Transported'] = pred
 samples['Transported'] = samples["Transported"].astype(bool)
 samples.to_csv('submission_bagging.csv',index=False)
 
-'''Calculated prediction'''
-print(pred.mean())
+# '''Calculated prediction'''
+# print(pred.mean())
 # print(samples)
+
+
+'''Kaggle Score:0.78957'''
